e36a2a05c53ef2b49d1d71cde314e9174a563124
Thiago Brezinski <thiagobrez@gmail.com>
2024-04-02 17:41:40 +0100

    feat: add example website to link

---
+++ example-website/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>
+          <a href="visionosDeepLinking://test/1" target="_blank">
+            Link to visionosDeepLinking://
+          </a>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
